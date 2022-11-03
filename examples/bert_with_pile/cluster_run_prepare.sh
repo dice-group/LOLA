4c561cf9a4128148bc70042543a959ccaa25ea29
Nikit Srivastava <nikit91@gmail.com>
2022-11-03 13:27:44 +0100

    modifying resource usage

--- examples/bert_with_pile/cluster_run_prepare.sh
+++ examples/bert_with_pile/cluster_run_prepare.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -J "Prepare the Pile Dataset"
-#SBATCH -N 8
-#SBATCH -n 8
-#SBATCH --cpus-per-task=15
+#SBATCH -N 15
+#SBATCH -n 15
+#SBATCH --cpus-per-task=4
 #SBATCH -p largemem
-#SBATCH --mem-per-cpu 16G
+#SBATCH --mem-per-cpu 20G
 ###SBATCH -q express
 ###SBATCH --gres=gpu:a100:4
 #SBATCH -t 15:00:00
@@ -16,6 +16,6 @@ echo "Total nodes: ${TOTAL_TASKS}"
 TOTAL_CHUNKS=30
 CHUNK_OFFSET=$((TOTAL_CHUNKS / TOTAL_TASKS))
 echo "Chunk offset: ${CHUNK_OFFSET}"
-#export $CHUNK_OFFSET
-#export $TOTAL_TASKS
+export CHUNK_OFFSET
+export TOTAL_TASKS
 srun ./run_prepare.sh
