f4417bd38d0d455ca2989126b4538d1be118a7fe
Denis <denkv@users.noreply.github.com>
2022-11-10 14:26:19 +0100

    Update the slurm options

--- examples/bert_with_pile/cluster_run_prepare.sh
+++ examples/bert_with_pile/cluster_run_prepare.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #SBATCH -J "Prepare the Pile Dataset"
-#SBATCH -N 15
-#SBATCH -n 15
-#SBATCH --cpus-per-task=4
+#SBATCH -N 10
+#SBATCH -n 10
+#SBATCH --cpus-per-task=1
 #SBATCH -p largemem
-#SBATCH --mem-per-cpu 20G
-###SBATCH -q express
-###SBATCH --gres=gpu:a100:4
-#SBATCH -t 15:00:00
+#SBATCH --mem-per-cpu 100G
+#SBATCH -t 100:00:00
 
 module load mpi/OpenMPI/4.1.4-GCC-11.3.0
 srun ./run_prepare.py
