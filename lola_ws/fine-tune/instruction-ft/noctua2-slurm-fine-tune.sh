e962b1210d34e7506541c02b05bd77db65077fe4
Nikit Srivastava <nikit91@gmail.com>
2024-03-27 20:35:18 +0100

    adding instructions fine-tuning scripts

---
+++ lola_ws/fine-tune/instruction-ft/noctua2-slurm-fine-tune.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -t 30:00:00
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem 250G
+#SBATCH -J "fine-tuning lola"
+#SBATCH --gres=gpu:a100:1
+#SBATCH --cpus-per-task=64
+
+
+# Loading required modules
+module load toolchain/foss/2022b
+module load lib/libaio/0.3.113-GCCcore-12.2.0
+module load lang
+module load Anaconda3
+module load system/CUDA/12.0.0
+module load lib/NCCL/2.16.2-GCCcore-12.2.0-CUDA-12.0.0
+module load compiler/GCC/10.3.0
+
+export VENV_PATH=/scratch/hpc-prf-lola/nikit/repos/LOLA-Megatron-DeepSpeed/lola_ws/lola-vsc-venv
+export LD_LIBRARY_PATH=$VENV_PATH/lib/python3.10/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH
+
+# conda activate $VENV_PATH
+
+source activate $VENV_PATH
+
+
+bash run-fine-tune.sh
---
+++ lola_ws/fine-tune/instruction-ft/run-fine-tune.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+torchrun --nnodes=1 --nproc_per_node=1 --master_port=4545 train.py \
+    --model_name_or_path mistralai/Mistral-7B-v0.1 \
+    --data_path ./alpaca_data.json \
+    --bf16 True \
+    --output_dir ./output_model \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 2000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --deepspeed default_offload_opt_param.json \
+    --tf32 True
+
+# python -u -m debugpy --wait-for-client --listen 0.0.0.0:12121 -m torch.distributed.run --nnodes=1 --nproc_per_node=1 --master_port=4545 train.py \
+#     --model_name_or_path mistralai/Mistral-7B-v0.1 \
+#     --data_path ./alpaca_data.json \
+#     --bf16 True \
+#     --output_dir ./output_model \
+#     --num_train_epochs 3 \
+#     --per_device_train_batch_size 8 \
+#     --per_device_eval_batch_size 4 \
+#     --gradient_accumulation_steps 1 \
+#     --evaluation_strategy "no" \
+#     --save_strategy "steps" \
+#     --save_steps 2000 \
+#     --save_total_limit 1 \
+#     --learning_rate 2e-5 \
+#     --weight_decay 0. \
+#     --warmup_ratio 0.03 \
+#     --deepspeed default_offload_opt_param.json \
+#     --tf32 True
