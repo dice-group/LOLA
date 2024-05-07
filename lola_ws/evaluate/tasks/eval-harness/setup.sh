93c69fd138ecf69e3228ec7d8248ff8da97f815a
Nikit Srivastava <nikit91@gmail.com>
2024-05-07 15:58:30 +0200

    updating setup

--- lola_ws/evaluate/tasks/eval-harness/setup.sh
+++ lola_ws/evaluate/tasks/eval-harness/setup.sh
@@ -1,20 +1,23 @@
+#! /bin/bash
+# Importing config variables
+. task.config
 
-EVAL_PATH= git clone https://github.com/EleutherAI/lm-evaluation-harness
-
-TASK=EleutherAI
 #SUB_TASK= task_name
 #PRETRAINED_MODEL= your_model
 
 #create and activate environment task
-conda create --prefix ./$TASK-eval python=3
+conda create --prefix ./$TASK_NAME-eval python=3
 
-conda activate ./$TASK-eval
+conda activate ./$TASK_NAME-eval
 
 
 #clone the eval repository
-git clone $EVAL_PATH
+git clone $TASK_GIT_REPOSITORY $REPO_DIR
 
-cd lm-evaluation-harness
+cd $REPO_DIR
+
+git checkout $TASK_GIT_HASH
+
 pip install -e .
 
 echo  'installation finished'
