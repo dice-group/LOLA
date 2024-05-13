4d4deb969b50f5e16921740d35e53f378f35e86d
Tatiana Moteu Ngoli <mtatiana@aimsammi.org>
2024-05-13 14:18:26 +0200

    adding okapi script

---
+++ lola_ws/evaluate/tasks/okapi/setup.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+# Importing config variables
+. task.config
+
+#SUB_TASK= task_name
+#PRETRAINED_MODEL= your_model
+
+#create and activate environment task
+conda create --prefix ./$TASK_NAME-eval python=3
+
+conda activate ./$TASK_NAME-eval
+
+
+#clone the eval repository
+git clone $TASK_GIT_REPOSITORY $REPO_DIR
+
+cd $REPO_DIR
+
+git checkout $TASK_GIT_HASH
+
+pip install  -e ".[multilingual]"
+
+
+#download rhe dataset
+bash scripts/download.sh
+
+echo  'installation finished'
