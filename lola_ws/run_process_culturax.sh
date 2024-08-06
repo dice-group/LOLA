86fcb178907e10067b7a886f357d39737ff885a7
Nikit Srivastava <nikit91@gmail.com>
2024-08-06 16:21:55 +0200

    updating the culturax processing file

--- lola_ws/run_process_culturax.sh
+++ lola_ws/run_process_culturax.sh
@@ -9,7 +9,9 @@ PROC_SCRIPT="process_culturax.sh"
 mkdir -p proc_logs/
 
 # add languages to exclude if any
-declare -a exc_values=("en" "ru" "es" "de" "fr" "zh" "it" "pt")
+## Sometimes we exclude the ones that have already been downloaded, or maybe require too much time to dowload
+# declare -a exc_values=("en" "ru" "es" "de" "fr" "zh" "it" "pt")
+declare -a exc_values=("none")
 
 #echo "Using for loop with glob pattern:"
 for dir in "$SEARCH_DIR"/*; do
