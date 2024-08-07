dcf44bcccacce5c65a0bb92895d568a317b3fb2a
denkv <denkv@users.noreply.github.com>
2024-08-07 16:23:28 +0200

    Use `lang` instead of `dataset_name`

--- lola_ws/moe_analysis/moe_analysis.py
+++ lola_ws/moe_analysis/moe_analysis.py
@@ -19,7 +19,7 @@ import lola_hf_model.modeling_lola_gpt2
 MAX_SEQ_LENGTH = 2048
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset-name', required=True, help='(example: en)')
+parser.add_argument('--lang', required=True, help='(example: en)')
 parser.add_argument('--rows-limit', type=int, required=True)
 parser.add_argument('--output-dir', type=pathlib.Path, required=True)
 parser.add_argument('--device', default='cuda:0')
@@ -28,12 +28,12 @@ args = parser.parse_args()
 tokenizer = transformers.AutoTokenizer.from_pretrained('dice-research/lola_v1')
 model = LOLALMHeadModel.from_pretrained('dice-research/lola_v1').to(args.device)
 
-dataset = datasets.load_dataset('uonlp/CulturaX', args.dataset_name, split='train', streaming=True)
+dataset = datasets.load_dataset('uonlp/CulturaX', args.lang, split='train', streaming=True)
 
 args.output_dir.mkdir(parents=True, exist_ok=True)
-with open(args.output_dir / f'row_data.{args.dataset_name}.data', 'w') as datafile, open(args.output_dir / f'token_experts.{args.dataset_name}.dat', 'w', newline='') as csvfile:
+with open(args.output_dir / f'row_data.{args.lang}.data', 'w') as datafile, open(args.output_dir / f'token_experts.{args.lang}.dat', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter='\t', quoting=csv.QUOTE_NONE)
-    for i, row in enumerate(tqdm.tqdm(itertools.islice(dataset, args.rows_limit), desc=args.dataset_name, total=args.rows_limit)):
+    for i, row in enumerate(tqdm.tqdm(itertools.islice(dataset, args.rows_limit), desc=args.lang, total=args.rows_limit)):
         experts_info = []
         lola_hf_model.modeling_lola_gpt2.expert_analysis_callback = lambda selected_experts: experts_info.append([e[0] for e in selected_experts])
         input_ids = tokenizer(row['text'], return_tensors='pt')['input_ids'][:, :MAX_SEQ_LENGTH].to(model.device)
@@ -44,7 +44,7 @@ with open(args.output_dir / f'row_data.{args.dataset_name}.data', 'w') as datafi
         tokens = len(input_ids[0])
         layers = len(experts_info)
         if i == 0:
-            writer.writerow(['dataset', 'row', 'token'] + [f'expert_layer_{layer}' for layer in range(layers)])
+            writer.writerow(['lang', 'row', 'token'] + [f'expert_layer_{layer}' for layer in range(layers)])
         datafile.write(json.dumps(row | {'index': i}) + '\n')
         for token in range(tokens):
-            writer.writerow([args.dataset_name, i, token] + [int(experts_info[layer][token]) for layer in range(layers)])
+            writer.writerow([args.lang, i, token] + [int(experts_info[layer][token]) for layer in range(layers)])
