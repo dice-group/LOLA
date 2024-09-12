22a912c99e1ca78567e1fa433be65085e6b5157b
Nikit Srivastava <nikit91@gmail.com>
2024-09-12 17:49:10 +0200

    updating const

--- lola_ws/evaluate/utils/consts.py
+++ lola_ws/evaluate/utils/consts.py
@@ -1,7 +1,7 @@
 # Note: the model ids in the sets below are made by conjoining huggingface organization and model name together with __ (similar to how lm-eval-harness does it)
 DEFAULT_MODELS = {'dice-research__lola_v1'}
 # Models to exclude
-EXCLUDED_MODELS = {'SeaLLMs__SeaLLMs-v3-1.5B-Chat', 'facebook__m2m100_1.2B'}
+EXCLUDED_MODELS = {'SeaLLMs__SeaLLMs-v3-1.5B-Chat', 'facebook__m2m100_1.2B', 'HuggingFaceH4__zephyr-7b-beta'}
 # All the incompatible task and language pairs
 INCOMPATIBLE_SUBTASKS_MODELS = {('belebele','facebook__mbart-large-50'),
                                 ('belebele','google__mt5-large'),
